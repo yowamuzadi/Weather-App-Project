064773880e537a67ea278f831ec4778d956c9e20
yowamuzadi <57663987+yowamuzadi@users.noreply.github.com>
2021-01-24 17:44:39 +0100

    Update app.js with handleSubmit function
    The goal is to get the weather info from the weather API and diplaying it when user submits a city

--- src/app.js
+++ src/app.js
@@ -42,3 +42,40 @@ function showCurrentDate(date) {
   let currentYearValue = now.getFullYear();
   currentDate.innerHTML = `${months[currentMonthValue]} ${currentDateValue}, ${currentYearValue}`;
 }
+
+// Getting the weather info from the weather API and diplaying it
+function displayWeather(response) {
+  let cityElement = document.querySelector("#city");
+  let cityResponse = `${response.data.name}`;
+  cityElement.innerHTML = `${cityResponse}`;
+
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperatureResponse = Math.round(response.data.main.temp);
+  temperatureElement.innerHTML = temperatureResponse;
+
+  let weatherDescriptionResponse = response.data.weather[0].main;
+  let weatherDescriptionElement = document.getElementById(
+    "weather-description"
+  );
+  weatherDescriptionElement.innerHTML = `${weatherDescriptionResponse}`;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#feels-like").innerHTML = Math.round(
+    response.data.main.feels_like
+  );
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+
+function searchCity(city) {
+  let weatherApiKey = "8165a17b0d39ff333ddf1c75c84ef1bb";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${weatherApiKey}`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.getElementById("city-input").value;
+  searchCity(city);
+}
